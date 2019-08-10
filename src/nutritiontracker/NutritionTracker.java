7cebbd9b9a7fc46bab37acf0b327a7038b97def3
Michal <zarnowsk@sheridancollege.ca>
2019-08-10 16:53:51 -0400

    Added comments

--- src/nutritiontracker/NutritionTracker.java
+++ src/nutritiontracker/NutritionTracker.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package nutritiontracker;
 
 import javafx.application.Application;
@@ -11,7 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
+/** Main class of the application containing main method
  *
  * @author Michal Zarnowski
  */
